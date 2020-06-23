20b80a2d0d19f6aaaa14e50f1976e16797644e73
hadymaher311 <hadymaher311@gmail.com>
2020-06-23 14:38:28 +0200

    add log menu

--- Mobile Application/TinyEye/components/NotificationCard.js
+++ Mobile Application/TinyEye/components/NotificationCard.js
@@ -1,31 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Card, Avatar, IconButton } from 'react-native-paper';
-import IconBadge from 'react-native-icon-badge';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Card, Avatar, IconButton, Menu, Divider } from "react-native-paper";
+import IconBadge from "react-native-icon-badge";
 
 const NotificationCard = (props) => {
-    const image = props.image;
-    const symbol = (props.type === 'known') ? "account-check" : "incognito";
-    return (
-        <Card.Title
-        title= {(props.type === 'known') ? "Family Member" : "Unknown"}
-        subtitle={"Entered your home !!"}
-        left={(props) => 
-          <View style={{flexDirection: 'row',alignItems: 'center',justifyContent: 'center',}}>
-			<IconBadge
-				MainElement={
-				<Avatar.Image size={50} source = {{uri : image}} style={{margin:6}}/>
-				}
-				BadgeElement={
-				<Avatar.Icon {...props} size={25} icon={symbol} />
-				}
-			/>
-		</View>}
-        right={(props) => <IconButton {...props} icon="dots-vertical" onPress={() => {}} />}
-      /> 
-    )
-}
+  const [visible, setVisible] = useState(false);
 
-export default NotificationCard
+  const _openMenu = () => {
+    console.log("pressed");
+    setVisible(true);
+  };
 
-const styles = StyleSheet.create({})
+  const _closeMenu = () => setVisible(false);
+
+  const image = props.image;
+  const symbol = props.type === "known" ? "account-check" : "incognito";
+  return (
+    <Card.Title
+      title={props.type === "known" ? "Family Member" : "Unknown"}
+      subtitle={"Entered your home !!"}
+      left={(props) => (
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <IconBadge
+            MainElement={
+              <Avatar.Image
+                size={50}
+                source={{ uri: image }}
+                style={{ margin: 6 }}
+              />
+            }
+            BadgeElement={<Avatar.Icon {...props} size={25} icon={symbol} />}
+          />
+        </View>
+      )}
+      right={(props) => (
+        <Menu
+          visible={visible}
+          onDismiss={_closeMenu}
+          anchor={
+            <IconButton {...props} onPress={_openMenu} icon="dots-vertical" />
+          }
+        >
+          <Menu.Item onPress={() => {}} title="Mark as Read" />
+          <Menu.Item onPress={() => {}} title="Delete" />
+        </Menu>
+      )}
+    />
+  );
+};
+
+export default NotificationCard;
+
+const styles = StyleSheet.create({});
--- Mobile Application/TinyEye/screens/HomeScreen.js
+++ Mobile Application/TinyEye/screens/HomeScreen.js
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import Background from "../components/Background";
 import { theme } from "../core/theme";
 import { Card, Avatar, IconButton, Badge } from "react-native-paper";
-import IconBadge from 'react-native-icon-badge';
+import IconBadge from "react-native-icon-badge";
 import NotificationCard from "../components/NotificationCard";
 
 export class HomeScreen extends Component {
@@ -12,68 +12,66 @@ export class HomeScreen extends Component {
     const { navigation } = this.props;
     const array = [
       {
-        key : '1',
-        type : 'known',
-        image : "https://picsum.photos/id/1011/200/300"
+        key: "1",
+        type: "known",
+        image: "https://picsum.photos/id/1011/200/300",
       },
       {
-        key : '2',
-        type : 'known',
-        image : "https://picsum.photos/id/1011/200/300"
+        key: "2",
+        type: "known",
+        image: "https://picsum.photos/id/1011/200/300",
       },
       {
-        key : '3',
-        type : 'known',
-        image : "https://picsum.photos/id/1011/200/300"
+        key: "3",
+        type: "known",
+        image: "https://picsum.photos/id/1011/200/300",
       },
       {
-        key : '4',
-        type : 'known',
-        image : "https://picsum.photos/id/1011/200/300"
+        key: "4",
+        type: "known",
+        image: "https://picsum.photos/id/1011/200/300",
       },
       {
-        key : '5',
-        type : 'unknown',
-        image : "https://picsum.photos/id/1005/200/300"
+        key: "5",
+        type: "unknown",
+        image: "https://picsum.photos/id/1005/200/300",
       },
       {
-        key : '6',
-        type : 'unknown',
-        image : "https://picsum.photos/id/1005/200/300"
+        key: "6",
+        type: "unknown",
+        image: "https://picsum.photos/id/1005/200/300",
       },
       {
-        key : '7',
-        type : 'unknown',
-        image : "https://picsum.photos/id/1005/200/300"
+        key: "7",
+        type: "unknown",
+        image: "https://picsum.photos/id/1005/200/300",
       },
       {
-        key : '8',
-        type : 'unknown',
-        image : "https://picsum.photos/id/1005/200/300"
+        key: "8",
+        type: "unknown",
+        image: "https://picsum.photos/id/1005/200/300",
       },
       {
-        key : '9',
-        type : 'unknown',
-        image : "https://picsum.photos/id/1005/200/300"
+        key: "9",
+        type: "unknown",
+        image: "https://picsum.photos/id/1005/200/300",
       },
-
     ];
     return (
       <Background>
-        <View style={{ marginTop: 80, marginBottom: 20 , alignItems: "center" }}>
-		{array.map((item) =>  
-			<NotificationCard 
-			image={item.image}
-			type={item.type}
-			/>
-		)}    
+        <View style={{ marginTop: 80, marginBottom: 20, alignItems: "center" }}>
+          {array.map((item) => (
+            <NotificationCard
+              key={item.key}
+              image={item.image}
+              type={item.type}
+            />
+          ))}
         </View>
       </Background>
     );
   }
 }
-  
-const styles = StyleSheet.create({
-  
-});
+
+const styles = StyleSheet.create({});
 export default HomeScreen;
